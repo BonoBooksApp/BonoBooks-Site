cfeb21bc1242c5b37f93b29b28de7efbf2dcb0d7
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-08-15 19:44:21 -0700

    Modified Video Image Sizes

--- pages/Embed.js
+++ pages/Embed.js
@@ -4,8 +4,6 @@ import styles from '../styles/embed.module.css';
 const Embed = (props) => (
   <div className={styles.card}>
     <iframe
-      width="853"
-      height="480"
       src={`https://www.youtube.com/embed/${props.embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
