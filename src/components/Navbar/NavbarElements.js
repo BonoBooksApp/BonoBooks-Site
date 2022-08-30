5ffa2b539826c7724f721a0e5839780306a37444
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2022-08-29 17:00:48 -0700

    Fixed navbar/sidebar sizing issue

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -24,7 +24,7 @@ export const NavContainer = styled.div`
     height: 60px;
     z-index: 1;
     width: 100%;
-    padding: 0 24px;
+    padding: 0;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08), 0px 4px 24px rgba(0, 0, 0, 0.08);
 `;
 
